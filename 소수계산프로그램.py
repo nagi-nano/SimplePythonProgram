6566562d5cf656cc2cdc3730ca0bda8ebddb1c41
nagi <66349084+nagi-nano@users.noreply.github.com>
2021-01-01 10:56:59 +0900

    Update 소수계산프로그램.py

--- 소수계산프로그램.py
+++ 소수계산프로그램.py
@@ -1,8 +1,25 @@
-number = 1000000
-for i in range(1, number):
-    count = 0
-    for j in range(1, i+1):
-        if i % j == 0:
-            count += 1
-    if count == 2:
-        print(i)
+import sys
+from pprint import pprint
+
+try:
+    number = int(sys.argv[1])
+except IndexError:
+    number = 10000
+
+
+def find_prime_number(start_number, end_number):
+    prime_numbers = []
+    for i in range(start_number, end_number):
+        count = 0
+        for j in range(2, i + 1):
+            if i % j == 0:
+                count += 1
+        if count > 1:
+            continue
+        elif count == 1:
+            prime_numbers.append(i)
+
+    return prime_numbers
+
+
+pprint(find_prime_number(1, number))
