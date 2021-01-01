93326e824c46dd888403122ba231bfe6b9672895
nagi <66349084+nagi-nano@users.noreply.github.com>
2021-01-01 11:52:28 +0900

    Update lotto_number_generator.py

--- lotto_number_generator.py
+++ lotto_number_generator.py
@@ -1,10 +1,10 @@
-from random import randrange
+from random import randint
 
 
 def lotto_number_generator():
     lotto_number = []
     while len(lotto_number) < 6:
-        new_lotto_number = randrange(1, 46)
+        new_lotto_number = randint(1, 45)
         if new_lotto_number in lotto_number:
             continue
         else:
